79388ea491aa11e4172ca87f2a08898a893bf75e
leukelele <luke.bk.le@gmail.com>
2024-04-20 16:33:42 -0500

    init code for listening to socket 500

--- src/main.py
+++ src/main.py
@@ -2,3 +2,15 @@ import time
 import socket
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(("0.0.0.0"m 5000))
+
+server,listen
+
+while True:
+    client, addr = server.accept()
+    print("Connection from ", addr)
+    client.send("you are connected!\n".encode())
+    client.send(f"{data['data'][:.0]}}\n".encode)())
+    time.sleep(2)
+    clinet.send("You are being disconnected!\n".encode())
+    clinet.close()
