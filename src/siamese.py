02b71bd248c80d1d459ed7a7889b9574be4e1a54
leukelele <luke.bk.le@gmail.com>
2024-04-26 06:44:08 -0500

    there was no need to comment out the visualize function but rather the function call

--- src/siamese.py
+++ src/siamese.py
@@ -133,68 +133,68 @@ x_test_2 = pairs_test[:, 1]
 ####################################################################
 ## Visualize pairs and their labels                                #
 ####################################################################
-#def visualize(pairs, labels, to_show=6, num_col=3, predictions=None, test=False):
-#    """Creates a plot of pairs and labels, and prediction if it's test dataset.
-#
-#    Arguments:
-#        pairs: Numpy Array, of pairs to visualize, having shape
-#               (Number of pairs, 2, 28, 28).
-#        to_show: Int, number of examples to visualize (default is 6)
-#                `to_show` must be an integral multiple of `num_col`.
-#                 Otherwise it will be trimmed if it is greater than num_col,
-#                 and incremented if if it is less then num_col.
-#        num_col: Int, number of images in one row - (default is 3)
-#                 For test and train respectively, it should not exceed 3 and 7.
-#        predictions: Numpy Array of predictions with shape (to_show, 1) -
-#                     (default is None)
-#                     Must be passed when test=True.
-#        test: Boolean telling whether the dataset being visualized is
-#              train dataset or test dataset - (default False).
-#
-#    Returns:
-#        None.
-#    """
-#
-#    # Define num_row
-#    # If to_show % num_col != 0
-#    #    trim to_show,
-#    #       to trim to_show limit num_row to the point where
-#    #       to_show % num_col == 0
-#    #
-#    # If to_show//num_col == 0
-#    #    then it means num_col is greater then to_show
-#    #    increment to_show
-#    #       to increment to_show set num_row to 1
-#    num_row = to_show // num_col if to_show // num_col != 0 else 1
-#
-#    # `to_show` must be an integral multiple of `num_col`
-#    #  we found num_row and we have num_col
-#    #  to increment or decrement to_show
-#    #  to make it integral multiple of `num_col`
-#    #  simply set it equal to num_row * num_col
-#    to_show = num_row * num_col
-#
-#    # Plot the images
-#    fig, axes = plt.subplots(num_row, num_col, figsize=(5, 5))
-#    for i in range(to_show):
-#        # If the number of rows is 1, the axes array is one-dimensional
-#        if num_row == 1:
-#            ax = axes[i % num_col]
-#        else:
-#            ax = axes[i // num_col, i % num_col]
-#
-#        ax.imshow(ops.concatenate([pairs[i][0], pairs[i][1]], axis=1), cmap="gray")
-#        ax.set_axis_off()
-#        if test:
-#            ax.set_title("True: {} | Pred: {:.5f}".format(labels[i], predictions[i][0]))
-#        else:
-#            ax.set_title("Label: {}".format(labels[i]))
-#    if test:
-#        plt.tight_layout(rect=(0, 0, 1.9, 1.9), w_pad=0.0)
-#    else:
-#        plt.tight_layout(rect=(0, 0, 1.5, 1.5))
-#    plt.show()
-#
+def visualize(pairs, labels, to_show=6, num_col=3, predictions=None, test=False):
+    """Creates a plot of pairs and labels, and prediction if it's test dataset.
+
+    Arguments:
+        pairs: Numpy Array, of pairs to visualize, having shape
+               (Number of pairs, 2, 28, 28).
+        to_show: Int, number of examples to visualize (default is 6)
+                `to_show` must be an integral multiple of `num_col`.
+                 Otherwise it will be trimmed if it is greater than num_col,
+                 and incremented if if it is less then num_col.
+        num_col: Int, number of images in one row - (default is 3)
+                 For test and train respectively, it should not exceed 3 and 7.
+        predictions: Numpy Array of predictions with shape (to_show, 1) -
+                     (default is None)
+                     Must be passed when test=True.
+        test: Boolean telling whether the dataset being visualized is
+              train dataset or test dataset - (default False).
+
+    Returns:
+        None.
+    """
+
+    # Define num_row
+    # If to_show % num_col != 0
+    #    trim to_show,
+    #       to trim to_show limit num_row to the point where
+    #       to_show % num_col == 0
+    #
+    # If to_show//num_col == 0
+    #    then it means num_col is greater then to_show
+    #    increment to_show
+    #       to increment to_show set num_row to 1
+    num_row = to_show // num_col if to_show // num_col != 0 else 1
+
+    # `to_show` must be an integral multiple of `num_col`
+    #  we found num_row and we have num_col
+    #  to increment or decrement to_show
+    #  to make it integral multiple of `num_col`
+    #  simply set it equal to num_row * num_col
+    to_show = num_row * num_col
+
+    # Plot the images
+    fig, axes = plt.subplots(num_row, num_col, figsize=(5, 5))
+    for i in range(to_show):
+        # If the number of rows is 1, the axes array is one-dimensional
+        if num_row == 1:
+            ax = axes[i % num_col]
+        else:
+            ax = axes[i // num_col, i % num_col]
+
+        ax.imshow(ops.concatenate([pairs[i][0], pairs[i][1]], axis=1), cmap="gray")
+        ax.set_axis_off()
+        if test:
+            ax.set_title("True: {} | Pred: {:.5f}".format(labels[i], predictions[i][0]))
+        else:
+            ax.set_title("Label: {}".format(labels[i]))
+    if test:
+        plt.tight_layout(rect=(0, 0, 1.9, 1.9), w_pad=0.0)
+    else:
+        plt.tight_layout(rect=(0, 0, 1.5, 1.5))
+    plt.show()
+
 #visualize(pairs_train[:-1], labels_train[:-1], to_show=4, num_col=4)
 #
 #visualize(pairs_val[:-1], labels_val[:-1], to_show=4, num_col=4)
